2dabca9bf340f2ded9898921b31133c7b786fce1
karart1981 <karapart1981@gmail.com>
2025-07-12 07:47:31 +0400

    I added  a section of services in home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Hero from '@/components/hero/Hero'
 import Partners from '@/components/autoSlider/AutoSlider';
+import ServicesSection from '@/components/servicesSection/ServicesSection';
+
 export default function Home() {
   return (
     <>
        <Hero />
        <Partners />
+       <ServicesSection />
     </>
   );
 }
